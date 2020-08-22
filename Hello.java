d33dc8c61b86731b63b585e713fdb7464f44c0c9
Liza <70043921+lizalamichhane@users.noreply.github.com>
2020-08-21 23:05:06 -0400

    Add files via upload
    This is the first CMSC 255 section 002 lab and its .java file.

---
+++ Hello.java
@@ -0,0 +1,17 @@
+/************************************************************************* Hello.java
+************************************************************************** Liza Lamichhane 
+* Liza Lamichhane
+* 08/19/2020
+* CMSC 255 Section 002 
+************************************************************************/
+
+public class Hello {
+	public static void main(String [] args) {
+		System.out.println("Liza Lamichhane") ;
+		System.out.println("Hello") ;
+		System.out.println("First Lab") ;
+		System.out.println("Date : 08/19/2020") ; 
+		System.out.println("CMSC 255 Section 002") ;
+		System.out.println() ;
+	}
+}
